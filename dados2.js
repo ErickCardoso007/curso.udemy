0c0cd9996a6daf0cf93bdb9d5e329c49bcc29c25
Erick Cardoso <cyber666king@gmail.com>
2022-02-28 19:23:52 -0300

    Finaliza aula(2) sobre dados.

---
+++ dados2.js
@@ -0,0 +1,10 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+let total = preco * (1 - desconto);
+console.log("O preço com desconto é de " + total);
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+
+console.log("\nProduto: " + nome + "\nCategoria: " + categoria + "\nPreço: " + preco + "\nDesconto: " + desconto);
--- dados4.js
+++
@@ -1,4 +0,0 @@
-const a = 10
-const a = a + 12;
-
-console.log(a);
