a9a69ebb490aafb51a9ea42f7aa0b9c3d24c128a
Erick Cardoso <cyber666king@gmail.com>
2022-03-18 08:15:50 -0300

    Conclui aula sobre comentários.

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,16 @@
+//Comentário de uma linha 
+console.log("Linha 1");
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+console.log("Linha 2");
+
+/*
+ *  Comentário de multiplas linhas.
+ * 
+ *  Intuito levado a estilização do mesmo. (dificilmente usado, não é necessário.)
+ * 
+ */
+console.log("Linha 3");
