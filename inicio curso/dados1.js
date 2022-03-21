410dc64f4de1f87ea6c61063cd9ebeffc99e6591
Erick Cardoso <cyber666king@gmail.com>
2022-03-21 18:25:13 -0300

    ajuste

---
+++ inicio curso/dados1.js
@@ -0,0 +1,11 @@
+let nome = "caneta";
+let quantidade = 10;
+let preco = 5.8;
+let imposto = 1.2;
+let precoFinal = preco + imposto;
+
+console.log("\nnome: " + nome);
+console.log("quantidade: " + quantidade);
+console.log("preco: " + preco);
+console.log("imposto: " + imposto);
+console.log("\n-Preço total: " + precoFinal);
---
+++ inicio curso/dados4.js
@@ -0,0 +1,7 @@
+const a = 10
+const a = a + 12;
+
+console.log(a);
+
+// Não funciona porque você está tentando mudar o valor de uma constante.
+// Não é possivel prosseguir com essa ação.
