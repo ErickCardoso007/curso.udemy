c6cf8aeda004a1c6c85d0aa127daafcb86bda616
Erick Cardoso <cyber666king@gmail.com>
2022-03-21 08:57:23 -0300

    Finaliza aula básica sobre tipagem fraca.

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,16 @@
+let qualquer = "Legal!"
+console.log(qualquer);
+console.log(typeof qualquer);
+
+console.log('----------')
+
+qualquer = 2.3123123
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//Evitar o uso de palavras genéricas ou siglas ao nomear sua variavel/constante!
+let valor = ''
+let numero = 1
+let pqp = false //Produto Químico Perigoso... kkk
+
+//Prefira códigos claros a linha de comentários 
