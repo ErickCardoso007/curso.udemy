21101f10e7c71700c7d04f19a89783b9c2362b63
Erick Cardoso <cyber666king@gmail.com>
2022-03-26 10:20:41 -0300

    finaliza aula bônus sobre Number

---
+++ Fundamentos/cuidadosNumber.js
@@ -0,0 +1,16 @@
+console.log(1 / 0);
+// Qualquer divisão com o número 0 retorna o resultado infinito.   
+
+console.log("Nossa!" * 4);
+/* Diferente de outras linguagens o Js não multiplica 
+string a não ser que a string for reconhecido como numero
+Ex:*/
+console.log("10" * 2);
+
+
+
+console.log(1.3 * 2.1);
+//Mesmo que seja dois números o Js não consegue fazer a multipliucação com precisão
+
+console.log((10).toFixed(1));
+//O número só será lido pelo .toFixed se o número estiver entre parenteses.
