89e30510ea77c6ab2102abe610e549a78a32e6f3
Erick Cardoso <cyber666king@gmail.com>
2022-03-27 11:05:21 -0300

    finaliza aula sobre string

---
+++ Fundamentos/strings.js
@@ -0,0 +1,10 @@
+const nome = "Erick"
+
+console.log(nome.charAt(3))
+console.log(nome.charAt(5))
+console.log(nome.indexOf("E"))
+console.log(nome.substring(0))
+console.log(nome.substring(2, 4))
+console.log('nome: '.concat(nome).concat('!'))
+console.log(nome.replace('x', 'E'))
+console.log('Eu, estou, cansado'.split(','))
