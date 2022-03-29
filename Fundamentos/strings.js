327173b59259f71434594d860e1fafffa27a5ec5
Erick Cardoso <cyber666king@gmail.com>
2022-03-28 21:54:23 -0300

    implementa aula sobre string.

--- Fundamentos/strings.js
+++ Fundamentos/strings.js
@@ -3,8 +3,11 @@ const nome = "Erick"
 console.log(nome.charAt(3)) // c
 console.log(nome.charAt(5)) // posição não existe
 console.log(nome.indexOf("E")) // 0
+
 console.log(nome.substring(1)) // rick
 console.log(nome.substring(1, 4)) //ric
+
 console.log('nome: '.concat(nome).concat('!')) // nome: Erick!
+
 console.log(nome.replace('E', 'xxx')) // xxxrick
 console.log('Eu, estou, cansado'.split(',')) // ['Eu', 'estou', 'cansado']
