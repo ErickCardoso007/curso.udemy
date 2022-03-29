1663c5919ea4bd0118b2faddc7c5edd9c255b968
Erick Cardoso <cyber666king@gmail.com>
2022-03-28 21:53:27 -0300

    Finaliza complementação da aula sobre string.

--- Fundamentos/strings.js
+++ Fundamentos/strings.js
@@ -1,10 +1,10 @@
 const nome = "Erick"
 
-console.log(nome.charAt(3))
-console.log(nome.charAt(5))
-console.log(nome.indexOf("E"))
-console.log(nome.substring(0))
-console.log(nome.substring(2, 4))
-console.log('nome: '.concat(nome).concat('!'))
-console.log(nome.replace('x', 'E'))
-console.log('Eu, estou, cansado'.split(','))
+console.log(nome.charAt(3)) // c
+console.log(nome.charAt(5)) // posição não existe
+console.log(nome.indexOf("E")) // 0
+console.log(nome.substring(1)) // rick
+console.log(nome.substring(1, 4)) //ric
+console.log('nome: '.concat(nome).concat('!')) // nome: Erick!
+console.log(nome.replace('E', 'xxx')) // xxxrick
+console.log('Eu, estou, cansado'.split(',')) // ['Eu', 'estou', 'cansado']
