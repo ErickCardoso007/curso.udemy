714fd588934be8cf0129e98768689befb8c9cddd
Erick Cardoso <cyber666king@gmail.com>
2022-11-01 10:06:32 -0300

    Finaliza aula de array

---
+++ Fundamentos/array.js
@@ -0,0 +1,6 @@
+const animais = ['Leão', 'cavalo', 'macaco']
+console.log('Animal escolhido: ' + animais[0]);
+animais.push('cobra', 'preguiça')
+console.log(animais);
+console.log(animais.pop());
+delete animais[0]
