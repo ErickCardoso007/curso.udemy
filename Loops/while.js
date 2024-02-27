2d925ad99634e759bc6ee15da4fad1851cf1939e
Cardoso zz <erickcardosofront@gmail.com>
2024-02-26 21:38:22 -0300

    finaliza aula de loop while.

---
+++ Loops/while.js
@@ -0,0 +1,17 @@
+// Conte ate 10
+// let i = 0
+
+// while(i < 10){
+//     console.log(i);
+//     i++
+// }
+
+// Tire os carros da garagem.
+
+let quantidadeDeCarros = 10
+
+while(quantidadeDeCarros != 0){
+    console.log(`faltam ${quantidadeDeCarros} ainda cara!`);
+    quantidadeDeCarros--
+}
+console.log('Perfeito! Nao restam mais carros.');
