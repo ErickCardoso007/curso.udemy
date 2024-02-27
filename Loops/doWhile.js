ad1a8e34dddec2048b0bc39a5fc4807f9f4e0ab0
ErickCardoso007 <cyber666king@gmail.com>
2024-02-27 20:40:33 -0300

    Finaliza exercicio de loop (do/while)

---
+++ Loops/doWhile.js
@@ -0,0 +1,8 @@
+let contagemRegressivaAnoNovo = 10 + 1
+
+do{
+    contagemRegressivaAnoNovo--
+    console.log('seu contador esta em: ' + contagemRegressivaAnoNovo);
+}while(contagemRegressivaAnoNovo > 1)
+
+console.log('Feliz Ano Novo!!!');
