dc2462a3230deae943d60a5d39e6d2fa4604e2f0
ErickCardoso007 <cyber666king@gmail.com>
2024-02-29 01:20:46 -0300

    Utiliza if ternario para fins de fixacao de conteudo

---
+++ condicionais/ifTernario.js
@@ -0,0 +1,4 @@
+let animaisNoZOO = ['cobra', 'passaro', 'porco'].length
+
+let condicaoDeEspaco = animaisNoZOO < 8 ? 'O Zologico suporta seus ' + animaisNoZOO + ' animais' : 'O Zologico nao suporta ' + animaisNoZOO + ' animais'
+console.log(condicaoDeEspaco);
