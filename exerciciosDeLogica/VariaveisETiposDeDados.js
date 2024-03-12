c7608ab5e4a7f4aa919e91c4a1747af7cc7fa642
ErickCardoso007 <cyber666king@gmail.com>
2024-03-11 21:26:24 -0300

    Finaliza exercicio de apresentacao utilizando objeto.

---
+++ exerciciosDeLogica/VariaveisETiposDeDados.js
@@ -0,0 +1,9 @@
+/*Escreva um programa que declare um objeto com os dados de uma pessoa sendo eles 'nome' e 'idade', e os imprima 
+em um console em uma frase que  diga “Olá, meu nome é [nome] e eu tenho [idade] anos” */
+
+let pessoa = {
+    nome: 'erick', 
+    idade: 10
+}
+
+console.log(`Olá, meu nome é ${pessoa.nome} e eu tenho ${pessoa.idade} anos.`);
