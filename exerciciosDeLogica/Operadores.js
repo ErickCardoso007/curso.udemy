c879c931274d81c3477458ceecbcf557142c67f4
ErickCardoso007 <cyber666king@gmail.com>
2024-03-11 21:36:47 -0300

    Finaliza exercicio logico de com operadores.

---
+++ exerciciosDeLogica/Operadores.js
@@ -0,0 +1,15 @@
+// Crie um programa que solicite ao usuário dois números e exiba o resultado da soma, subtração, multiplicação e divisão desses números.
+
+const calc = (a, b) => {
+    let soma = a + b
+    console.log(soma);
+    let sub = a - b
+    console.log(sub);
+    let multi = a * b
+    console.log(multi);
+    let div = a / b
+    console.log(div);
+
+}
+
+calc(2, 3)
