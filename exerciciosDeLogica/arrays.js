f1afcb872280d3b5975f2ec20e27200ca74fe725
ErickCardoso007 <cyber666king@gmail.com>
2024-03-11 22:04:00 -0300

    Finaliza exercicio de logica utilizando for e array.

---
+++ exerciciosDeLogica/arrays.js
@@ -0,0 +1,7 @@
+// Escreva um programa que declare um array de suas frutas favoritas e imprima cada fruta na lista usando um loop.
+
+let array = ['melancia', 'laranja', 'maca', 'banana', 'manga']
+
+for (let i = 0; array >= array[0]; i++) {
+    console.log(array.shift());
+}
