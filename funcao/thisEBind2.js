a4d0c5b2604dc42c7f1fd4408d9f8012653758d5
ErickCardoso007 <cyber666king@gmail.com>
2024-03-20 16:58:40 -0300

    Utliza arrrow function para evitar que o this aponte para outro objeto.

--- funcao/thisEBind2.js
+++ funcao/thisEBind2.js
@@ -2,10 +2,10 @@ function Pessoa() {
     this.idade = 0
 
     const self = this
-    setInterval(function() {
+    setInterval(() => {
         self.idade++
         console.log(self.idade);
     }, 1000)
 }
 
-new Pessoa
+new Pessoa                                                     
