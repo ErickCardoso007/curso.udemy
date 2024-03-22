4b4a0bba6e5a9f9f8aff92da6f7adcab2909d33a
ErickCardoso007 <cyber666king@gmail.com>
2024-03-22 18:16:16 -0300

    Finaliza exercicio utilizando function factory.

---
+++ funcao/factoryExercicio.js
@@ -0,0 +1,13 @@
+function pessoa(nome, idade, salario, cargo, tempoNaEmpresa){
+    return {
+        nome: nome,
+        idade: idade, 
+        salario: salario,
+        cargo: cargo, 
+        tempoNaEmpresa: tempoNaEmpresa
+    }
+    
+}
+
+console.log(pessoa('Erick', 18, 1.505, 'Sushiman', '16 meses'));
+
