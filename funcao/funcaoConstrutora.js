3011accc15b1c6924a157a3457262c01acb37158
ErickCardoso007 <cyber666king@gmail.com>
2024-03-22 19:19:59 -0300

    Refaz aula sobre funcao contrutora.

--- funcao/funcaoConstrutora.js
+++ funcao/funcaoConstrutora.js
@@ -1,3 +1,16 @@
-const preco = 10
-const desconto = 0.19
-console.log((preco - desconto));
+function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    
+    this.apresentar = function() {
+        console.log("Olá, meu nome é " + this.nome + " e tenho " + this.idade + " anos.");
+    };
+}
+
+
+const pessoa1 = new Pessoa("João", 30);
+const pessoa2 = new Pessoa("Maria", 25);
+
+
+pessoa1.apresentar(); 
+pessoa2.apresentar(); 
