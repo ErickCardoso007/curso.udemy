180bf7aebb6a438f6e26590da0fc5b9951dd6d7a
ErickCardoso007 <cyber666king@gmail.com>
2024-04-02 20:07:22 -0300

    Finaliza exercicio sobre potenciacao.

---
+++ exercicioUdemy/exercicio3.js
@@ -0,0 +1,9 @@
+// Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+
+function retornaPonteciacao(base, expoente){
+    let result = Math.pow(base, expoente)
+    return result
+}
+
+console.log(retornaPonteciacao(2, 3));
