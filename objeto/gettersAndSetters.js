73cc43e3d18b6f4417df81267ded4131ab290774
ErickCardoso007 <cyber666king@gmail.com>
2024-05-31 18:51:22 -0300

    Finaliza aula sobre getters/setters.

---
+++ objeto/gettersAndSetters.js
@@ -0,0 +1,15 @@
+const obj = {
+    animais: ['cachorro', 'gato', 'passarinho'],
+    get primeiroAnimal(){
+        return this.animais[0]
+    },
+    set primeiroAnimal(item){
+        this.animais.unshift(item)
+    }
+
+}
+
+console.log(obj.primeiroAnimal);
+obj.primeiroAnimal = 'Macaco'
+
+console.log(obj.animais.sort());
