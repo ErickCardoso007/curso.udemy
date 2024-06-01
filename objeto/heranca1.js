a021d239ea6dd37358cda23b9a3632cd23694e0f
ErickCardoso007 <cyber666king@gmail.com>
2024-06-01 18:40:28 -0300

    Correcao da aula sobre heranca 1.

--- objeto/heranca1.js
+++ objeto/heranca1.js
@@ -1,1 +1,22 @@
- // aula mais teorica, nn houve codigo.
+const animal = {
+    idade: 0,
+    som: '',
+    fazerSom(){
+        return this.som
+    }
+}
+
+const cachorro = {
+    nome: 'Aspas',
+    idade: 10,
+    som: 'AU Au Au'
+}
+const gato = {
+    nome: 'felix',
+    idade: 11,
+    som: 'miau'
+}
+Object.setPrototypeOf(cachorro, animal)
+Object.setPrototypeOf(gato, animal)
+console.log(`O gato chamado ${gato.nome} fez este som: ${gato.fazerSom()}`);
+console.log(`O cachorro chamado ${cachorro.nome} fez este som: ${cachorro.fazerSom()}`);
