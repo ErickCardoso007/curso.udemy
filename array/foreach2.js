fa589c7f6e6981a6d31c3abea6d0da2e4da9d489
ErickCardoso007 <cyber666king@gmail.com>
2024-06-04 17:17:56 -0300

    Finaliza segunda aula sobre foreach.

---
+++ array/foreach2.js
@@ -0,0 +1,12 @@
+
+
+const aprovados = ['Paulo', 'Ana', 'jeff']
+
+function forEach2(e){
+    for (let i = 0; i < e.length; i++) {
+        console.log(`${i + 1}) ${e[i]}`);
+        
+    }
+}
+
+forEach2(aprovados)
